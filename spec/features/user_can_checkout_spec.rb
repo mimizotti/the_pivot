ec36d03e1e19d56cd5846aab53df9dc6b2e0fb67
snayrouz <samuel.nayrouz@gmail.com>
2017-09-16 10:33:38 -0600

    Add user story 15 test

---
+++ spec/features/user_can_checkout_spec.rb
@@ -0,0 +1,73 @@
+RSpec.feature "user can checkout" do
+  scenario "from the cart path" do
+    User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive")
+    item_1 = category.items.create!(title: "Mask", description: "This is for your face", price: 10.00, image: "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.scubadivingdreams.com%2Fwp-content%2Fuploads%2F2015%2F11%2Fthe-best-scuba-snorkel-mask-mares-i3-sunrise.jpg")
+    item_2 = category.items.create!(title: "Mask", description: "This is for your face", price: 10.00, image: "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.scubadivingdreams.com%2Fwp-content%2Fuploads%2F2015%2F11%2Fthe-best-scuba-snorkel-mask-mares-i3-sunrise.jpg")
+
+    visit items_path
+
+    within(".item-#{item_1.id}") do
+      find(:css, ".add-to-cart").click
+    end
+
+    within(".item-#{item_2.id}") do
+      find(:css, ".add-to-cart").click
+    end
+
+    find(:css, ".cart").click
+
+    expect(current_path).to eq(cart_path)
+    expect(page).to have_content(item_1.title)
+    expect(page).to have_content(item_2.title)
+
+
+    within("nav") do
+      click_link("Login")
+    end
+
+    fill_in "session_username", with: user.username
+    fill_in "session_password", with: user.password
+    click_button("Login")
+
+    expect(current_path).to eq("/dashboard")
+
+    expect(page).to have_content("Logged in as #{user.username}")
+    expect(page).to have_content user.username
+
+    find(:css, ".cart").click
+
+    click_link "Checkout"
+
+    expect(current_path).to eq(orders_path)
+    expect(page).to have_content("Order was successfully placed")
+    expect(page).to have_content("order-#{user.orders.last.id}")
+
+    find(:css, ".cart").click
+
+    expect(page).to_not have_content(item_1.title)
+    expect(page).to_not have_content(item_2.title)
+  end
+
+  scenario "'checkout' should not display with 0 items in cart'" do
+    User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive")
+
+    visit items_path
+
+    within("nav") do
+      click_link("Login")
+    end
+
+    fill_in "session_username", with: user.username
+    fill_in "session_password", with: user.password
+    click_button("login")
+
+    expect(current_path).to eq("/dashboard")
+
+    expect(page).to have_content("Logged in as #{user.username}")
+    expect(page).to have_content user.username
+
+    find(:css, ".cart").click
+
+    expect(page).to_not have_content("Checkout")
+  end
+end
