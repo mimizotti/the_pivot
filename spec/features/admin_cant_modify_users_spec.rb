19c735fd308f39c044f7a7e4aedb697865af69e8
snayrouz <samuel.nayrouz@gmail.com>
2017-09-17 12:20:35 -0600

    Add container fluid class for formatting item show page

--- spec/features/admin_cant_modify_users_spec.rb
+++ spec/features/admin_cant_modify_users_spec.rb
@@ -1,8 +1,8 @@
 RSpec.feature "Admin edits the user" do
   scenario "Admin can edit their own account" do
 
-    admin = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: 1)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit admin_dashboard_path
 
@@ -22,8 +22,8 @@ RSpec.feature "Admin edits the user" do
 
   scenario "Cannot modify any other users account" do
 
-    admin = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: 1)
-    user = user_2 = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: 0)
+    user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+    user_2 = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit edit_account_path
