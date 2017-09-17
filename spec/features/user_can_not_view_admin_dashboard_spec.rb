6867df21f1565111882512eb53331b3c9b103036
Boots2291 <buteramv@gmail.com>
2017-09-16 22:58:52 -0600

    Assign variable to user on test to make it pass

--- spec/features/user_can_not_view_admin_dashboard_spec.rb
+++ spec/features/user_can_not_view_admin_dashboard_spec.rb
@@ -1,7 +1,7 @@
 RSpec.feature "User tried to go to admin dashboard" do
   scenario "gets a 404 error" do
 
-    User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive")
+    user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit admin_dashboard_path
