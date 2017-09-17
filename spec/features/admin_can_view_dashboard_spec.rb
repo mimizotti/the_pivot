943a90ffc12e9f4aaafab942716588ef1edd9a6e
Becki Srofe <blf_23@yahoo.com>
2017-09-17 14:56:10 -0600

    add variable to admin view dashboard

--- spec/features/admin_can_view_dashboard_spec.rb
+++ spec/features/admin_can_view_dashboard_spec.rb
@@ -1,7 +1,7 @@
 RSpec.feature "Admin visits dashboard" do
   scenario "admin sees dashboard" do
 
-    User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+    user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit admin_dashboard_path
@@ -14,7 +14,7 @@ RSpec.feature "Admin visits dashboard" do
     item_2 = category.items.create!(title: "Fin", description: "This is for your face", price: 10.00, image: "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.scubadivingdreams.com%2Fwp-content%2Fuploads%2F2015%2F11%2Fthe-best-scuba-snorkel-mask-mares-i3-sunrise.jpg")
     item_3 = category.items.create!(title: "Sunscreen", description: "This is for your face", price: 6.00, image: "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.scubadivingdreams.com%2Fwp-content%2Fuploads%2F2015%2F11%2Fthe-best-scuba-snorkel-mask-mares-i3-sunrise.jpg")
 
-    User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+    user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit root_path
