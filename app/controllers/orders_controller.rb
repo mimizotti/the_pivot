c159cf155297fcd8fe2428a9f30ed147f8696618
sidewinder2020 <sdwinder1@gmail.com>
2017-09-18 07:52:05 -0600

    Create new on orders controller, add button on cart show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,13 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def new
+		order = Order.create(status: "ordered", user_id: current_user.id)
+		item_hash = @cart.item_and_quantity
+		order.add(item_hash)
+		@cart.destroy
+		flash[:success] = "Congratulations on your successful underwater basketweaving purchase!"
+		redirect_to orders_path
+	end
 end
