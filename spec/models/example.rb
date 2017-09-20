8e018b2733c7805bf4ffa38ba462826abc5e6da3
Becki Srofe <blf_23@yahoo.com>
2017-09-20 16:15:53 -0600

    Add practice model

---
+++ spec/models/example.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe Job do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a title" do
+        job = Job.new(level_of_interest: 80, description: "Wahoo", city: "Denver")
+        expect(job).to be_invalid
+      end
+
+      it "is invalid without a level of interest" do
+        job = Job.new(title: "Developer", description: "Wahoo", city: "Denver")
+        expect(job).to be_invalid
+      end
+
+      it "is invalid without a city" do
+        job = Job.new(title: "Developer", description: "Wahoo", level_of_interest: 80)
+        expect(job).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with a title and level of interest" do
+        job = Job.new(title: "Developer", level_of_interest: 40, city: "Denver")
+        expect(job).to be_valid
+      end
+    end
+  end
+
+  describe "relationships" do
+    it "belongs to a company" do
+      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
+      expect(job).to respond_to(:company)
+    end
+  end
+end
