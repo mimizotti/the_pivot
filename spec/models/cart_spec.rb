70626a78188e32ea53a8a2f902e9d969f684cf6d
snayrouz <samuel.nayrouz@gmail.com>
2017-09-20 22:07:32 -0600

    Add cart spec skeleton

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,81 @@
+require "rails_helper"
+
+
+RSpec.describe Cart, type: :model do
+  describe "instance methods" do
+
+    describe "#add_item" do
+      it 'can add an item to the cart' do
+
+      end
+    end
+
+    describe "#count_of" do
+      it 'can display its quantity' do
+
+      end
+    end
+
+    describe "#total_count" do
+      it 'can count the total cart' do
+
+      end
+    end
+
+    describe "#cart_total"
+      it 'can display the cart total' do
+
+      end
+    end
+
+    describe "#quantity" do
+      it 'can display quantity by item id' do
+
+      end
+    end
+
+    describe "#increase_item" do
+      it 'can increase the quanity of the item by id' do
+
+      end
+    end
+
+    describe "#decrease_item" do
+      it 'can descrease the quanity of the item by id' do
+
+      end
+    end
+
+    describe "#remove_item" do
+      it 'can remove an item by id' do
+
+      end
+    end
+
+    describe "#parse_for_orders" do
+      it 'can parse through orders' do
+
+      end
+    end
+
+    describe "#order_hash" do
+      it 'can display the order in a hash' do
+
+      end
+    end
+
+    describe "#delete_item" do
+      it 'can delete an item from the cart' do
+
+      end
+    end
+
+    describe "#destroy" do
+      it 'can destroy its entire cart' do
+
+      end
+    end
+
+
+  end
+end
