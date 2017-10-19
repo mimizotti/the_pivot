1b337e03a53f9397bd1075f1d26e7af869ddfb9f
Liam Barstad <liambarstad@gmail.com>
2017-10-18 21:14:51 -0600

    add seedpeppers.rb and seed

--- db/seeds/pepper.rb
+++ db/seeds/pepper.rb
@@ -1,7 +1,13 @@
 class SeedPepper
 
   def seed
-    store = Store.create(name: "Fuko")
+    store = Store.create(name: "Fuko", description: "Feast your eyes on the spiciest peppers the world has to offer", image: "http://teriacha.com/wp-content/uploads/2016/09/Pepper.png")
+    32.times do |i|
+      28.times do |i|
+        store.items.create(title: titles[i].upcase, description: BetterLorem.w, price: rand(100..250))
+      end
+      store.items.create(title: "Black Pepper#{i}", description: "The most peppery", price: 999)
+    end
   end
 
   def titles
