f0721b007f08aa5cfc2c51c71bd3988c32111461
Liam Barstad <liambarstad@gmail.com>
2017-10-19 17:47:42 -0600

    add test for links to show pages

--- spec/features/categories/user_can_see_categories_index_spec.rb
+++ spec/features/categories/user_can_see_categories_index_spec.rb
@@ -33,4 +33,20 @@ RSpec.describe "user visits categories index path" do
     expect(page).to have_css("img[src*='#{category2.items.first.image}']")
   end
 
+  it "and will be able to navigate to show page by clicking on the icons" do
+    category1, category2 = create_list(:category, 2)
+    store1, store2 = create_list(:store, 2)
+    item1, item3 = create_list(:item, 2, store: store1)
+    item2 = create(:item, store: store2)
+
+    category1.items << item1
+    category1.items << item2
+    category2.items << item3
+    visit("/categories")
+    click_link(category1.name)
+
+    expect(current_path).to eq("/#{category1.name.downcase}")
+    expect(page).to have_content(category1.name)
+  end
+
 end
