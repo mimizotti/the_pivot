7a1707283c5dff150d280372a98e53c64b49ffcb
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-19 16:43:08 -0600

    add test to actually test retire_item function, will change to update action in controller and push logic down stack

--- spec/features/items/items_can_be_retired_spec.rb
+++ spec/features/items/items_can_be_retired_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "An item can be retired" do
-
+RSpec.describe "Admin visits item show" do
   let(:store) { Store.create(name: "Knautical Knots", description: "Underwater basket weaving supplies, not just for lazy millenials!", image: "knotical-knots.png") }
-
+  let(:admin) { create(:user, role: 1) }
+  context "and can retire an item" do
   it "and when a user views it they cannot add it to cart" do
-    item_1 = Item.create(title: "Mask", store: store, description: "Something to put on your face.", price: 49.95, status: 1, image: "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.scubadivingdreams.com%2Fwp-content%2Fuploads%2F2015%2F11%2Fthe-best-scuba-snorkel-mask-mares-i3-sunrise.jpg")
+    item_1 = Item.create(title: "Mask", store: store, description: "Something to put on your face.", price: 49.95, status: 0, image: "https://slack-imgs.com/?c=1&url=http%3A%2F%2Fwww.scubadivingdreams.com%2Fwp-content%2Fuploads%2F2015%2F11%2Fthe-best-scuba-snorkel-mask-mares-i3-sunrise.jpg")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit item_path(item_1)
+
+    click_on "Toggle Retire"
 
     expect(page).to have_content(item_1.title)
     expect(page).to have_content(item_1.description)
     expect(page).to have_content(item_1.price)
     expect(page).to_not have_content("Add to Cart")
     expect(page).to have_content("Item Retired")
+  end
   end
 end
