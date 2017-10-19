815723236dc103fa551c16f6620e87cc6014fdc8
Liam Barstad <liambarstad@gmail.com>
2017-10-18 20:53:09 -0600

    add SeedPepper class

---
+++ db/seeds/pepper.rb
@@ -0,0 +1,42 @@
+class SeedPepper
+
+  def seed
+    store = Store.create(name: "Fuko")
+  end
+
+  def titles
+    ["naga viper",
+     "trinidad moruga scorpion",
+     "carolina reaper",
+     "ghost pepper",
+     "scotch bonnet",
+     "serrano",
+     "datil pepper",
+     "habanero pepper",
+     "cayenne pepper",
+     "jalapeno pepper",
+     "anaheim chilli",
+     "sriracha pepper",
+     "chipotle",
+     "giardinera mix",
+     "aleppo pepper",
+     "pimento pepper",
+     "banana pepper",
+     "guajillo chilli",
+     "malaguenta pepper",
+     "bhut jolokia",
+     "dragon's breath",
+     "komodo dragon",
+     "barrackpore",
+     "desert naga pepper",
+     "naga monich pepper",
+     "chocolate habanero",
+     "devil's tongue",
+     "white bullet",
+     "guyana pepper"]
+  end
+
+
+
+
+end
