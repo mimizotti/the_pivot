baefd165d0d710382ee938fb519907f079b03a25
Dev Workstation <EMAIL@gmail.com>
2017-10-20 16:10:32 -0600

    dropped roles from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018223958) do
+ActiveRecord::Schema.define(version: 20171020220617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20171018223958) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 0
   end
 
   add_foreign_key "item_categories", "categories"
