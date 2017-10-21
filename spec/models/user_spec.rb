625f3211d18fc9755963f10ffbe7095dab447b7d
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-21 14:40:37 -0600

    update user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe User, type: :model do
-  describe 'validations' do
+describe User do
+  describe "validations" do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:username) }
-    it { should validate_presence_of(:password) }
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
     it { should validate_uniqueness_of(:username) }
     it { should have_many(:orders) }
   end
@@ -15,14 +15,14 @@ describe User, type: :model do
   describe "methods" do
     context ".full_name" do
       it "can generate a full name for a user" do
-        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", platform_admin: true)
         expect(user.full_name).to eq("Bon Jovi")
       end
     end
 
     context ".create_reset_digest" do
       it "generates a random 6 digit number and stores it on the user" do
-        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", platform_admin: true)
         expect(user.create_reset_digest.length).to eq 6
         expect(user.create_reset_digest).to be_a String
       end
