94eea8941bcbcfb4a0496dd9f261fc28614ccf12
Jim Szalewski <jim.szalewski@gmail.com>
2017-10-21 14:40:37 -0600

    update user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,65 +2,27 @@ require 'rails_helper'
 
 describe User do
   describe "validations" do
-    context "invalid attributes" do
-      it "is invalid without a first_name" do
-        user = User.new(last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive")
-        expect(user).to be_invalid
-      end
-
-      it "is invalid without a last_name" do
-        user = User.new(first_name: "Bon", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
-        expect(user).to be_invalid
-      end
-
-      it "is invalid without an address" do
-        user = User.new(first_name: "Bon", last_name: "Jovi", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
-        expect(user).to be_invalid
-      end
-
-      it "is invalid without an email" do
-        user = User.new(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", username: "bonjovirules", password: "deadoralive", role: "admin")
-        expect(user).to be_invalid
-      end
-
-      it "is invalid without a unique username" do
-        User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
-        user = User.new(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
-        expect(user).to be_invalid
-      end
-
-      it "is invalid without a password" do
-        user = User.new(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", role: "admin")
-        expect(user).to be_invalid
-      end
-    end
-
-    context "valid attributes" do
-      it "is valid with a first_name, last_name, address, email, username, and password" do
-        user = User.new(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
-        expect(user).to be_valid
-      end
-    end
-  end
-
-  describe "relationships" do
-    it "has many orders" do
-      user = User.new(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
-      expect(user).to respond_to(:orders)
-    end
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_uniqueness_of(:username) }
+    it { should have_many(:orders) }
   end
 
   describe "methods" do
     context ".full_name" do
       it "can generate a full name for a user" do
-        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", platform_admin: true)
         expect(user.full_name).to eq("Bon Jovi")
       end
     end
 
     context ".create_reset_digest" do
       it "generates a random 6 digit number and stores it on the user" do
-        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", role: "admin")
+        user = User.create(first_name: "Bon", last_name: "Jovi", address: "123 crazy street", email: "deadoralive@awesome.com", username: "bonjovirules", password: "deadoralive", platform_admin: true)
         expect(user.create_reset_digest.length).to eq 6
         expect(user.create_reset_digest).to be_a String
       end
