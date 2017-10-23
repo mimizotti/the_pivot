afa3ee1337cbed1cffe6be06026ef415995f162a
Dev Workstation <EMAIL@gmail.com>
2017-10-23 14:09:09 -0600

    updated business admin dashboard

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -7,5 +7,38 @@ class OrderItem < ApplicationRecord
   def self.store_orders(store)
     joins(:item).where(items: {store: store})
   end
+
+  def self.ordered
+    where(status: 0)
+  end
+
+  def self.paid
+    where(status: 1)
+  end
+
+  def self.cancelled
+    where(status: 2)
+  end
+
+  def self.completed
+    where(status: 3)
+  end
+
+  def self.ordered_count
+    where(status: 0).count
+  end
+
+  def self.paid_count
+    where(status: 1).count
+  end
+
+  def self.cancelled_count
+    where(status: 2).count
+  end
+
+  def self.completed_count
+    where(status: 3).count
+  end
+
 end
 
