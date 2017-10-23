7a02cb4acf2f9bf06b951815b6a4b33818e72de6
Liam Barstad <liambarstad@gmail.com>
2017-10-23 14:09:06 -0600

    for branch switching

--- app/controllers/api/v1/stores/categories_controller.rb
+++ app/controllers/api/v1/stores/categories_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Stores::CategoriesController < ApplicationController
 
   def index
-    render json: "hello"
+    render json: Category.first
     # token = request.headers['X_TOKEN']
     # if JsonTokenHandler.authenticate_token(token)
     #   store_name = request.params(:store)
