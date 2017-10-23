8236ba605873155c796d95d9b52774b9fd129d1a
Dev Workstation <EMAIL@gmail.com>
2017-10-23 13:08:35 -0600

    add status to order_item table

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,8 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
+  enum status: [:ordered, :paid, :cancelled, :completed]
+
   def self.store_orders(store)
     joins(:item).where(items: {store: store})
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020221942) do
+ActiveRecord::Schema.define(version: 20171023183937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20171020221942) do
     t.decimal "line_item_total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["item_id"], name: "index_order_items_on_item_id"
     t.index ["order_id"], name: "index_order_items_on_order_id"
   end
