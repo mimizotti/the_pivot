b21057dbdffb50e4912bc06079cd9225533c4d54
Liam Barstad <liambarstad@gmail.com>
2017-10-26 17:49:20 -0600

    add happy/sad tests for store.check_image

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Store, type: :model do
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:image) }
   it { should validate_uniqueness_of(:name) }
+
+  scenario "can check the image" do
+
+    it "with a valid image" do
+      store = create(:store, image: "http://imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_nikkor28-300mmf_35-56gd_ed_vr/img/sample/sample4_l.jpg")
+
+      expect(store.check_image).to be_truthy
+    end
+
+    it "with an invalid image" do
+      store = create(:store, image: "random string")
+
+      expect(store.check_image).to be_truthy
+    end
+
+  end
+
 end
