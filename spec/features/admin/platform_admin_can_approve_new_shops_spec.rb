2d82a4e15167ea21034ae8a37601282a530e239e
Liam Barstad <liambarstad@gmail.com>
2017-10-26 17:15:59 -0600

    add test to remove pending store when approved or rejected

--- spec/features/admin/platform_admin_can_approve_new_shops_spec.rb
+++ spec/features/admin/platform_admin_can_approve_new_shops_spec.rb
@@ -22,14 +22,6 @@ feature "Platform Admin" do
     click_button "Login"
   end
 
-  # As a user,
-  # When I am logged in as a platform admin, and I navigate to /dashboard
-  # I should see a link to see pending business requests
-  # When I click on that link, I should see a list of pending requests
-  # I should see a button to approve each request or decline each request
-  # And then that store will be listed or unlisted on site depending approval
-  #
-
   scenario "I can approve a new business for my website" do
     @store = create(:store, status: "pending")
     datetime = @store.created_at
@@ -44,6 +36,8 @@ feature "Platform Admin" do
     click_on("Approve Business")
 
     expect(@store.pending?).to be_falsy
+    expect(page).to_not have_content(@store.name)
+    expect(page).to_not have_content("Request made on #{datetime.month}/#{datetime.day}/#{datetime.year.to_s[2..3]}")
   end
 
   scenario "I can deny a new business application for my website" do
@@ -60,7 +54,8 @@ feature "Platform Admin" do
     click_on("Reject Business")
 
     expect(@store.pending?).to be_truthy
+    expect(page).to_not have_content(@store.name)
+    expect(page).to_not have_content("Request made on #{datetime.month}/#{datetime.day}/#{datetime.year.to_s[2..3]}")
   end
-
 
 end
