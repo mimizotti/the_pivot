0be5145fb54f72650e7ce879aa8d4003d5c05f8e
Liam Barstad <liambarstad@gmail.com>
2017-10-26 14:51:48 -0600

    add working image validation for store creation

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -15,9 +15,17 @@ class StoresController < ApplicationController
   def create
     @store = Store.new(store_params)
     @store.status = "pending"
-    @store.save!
-    redirect_to stores_path
-    flash[:message] = "Thank you for your submission, if your store is approved, then you'll see it here!"
+    img_response = Net::HTTP.get_response(URI.parse(params[:store][:image]))
+    if img_response.code == "200" && @store.save!
+      redirect_to stores_path
+      flash[:message] = "Thank you for your submission, if your store is approved, then you'll see it here!"
+    elsif img_response.code == "200"
+      redirect_to new_store_path
+      flash[:message] = "Invalid submission"
+    else
+      redirect_to new_store_path
+      flash[:message] = "Image file must be tenable"
+    end
   end
 
 
