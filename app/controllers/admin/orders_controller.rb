3bea15ac4659e264a497d28753cff45747cd6fd7
Ashish Vaid <vaid50@yahoo.com>
2017-10-26 14:04:50 -0600

    orders index and order by status order now paginated

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,10 +4,15 @@ class Admin::OrdersController < ApplicationController
   def index
     @store = Store.find(params["id"])
     @store_orders = OrderItem.includes(:item, order: :user).store_orders(@store)
+    @paginated_orders = @store_orders.paginate(:page => params[:page], :per_page => 50)
   end
 
   def ordered
-    @orders = Order.ordered
+
+    @orders = Order.ordered.paginate(:page => params[:page], :per_page => 50)
+    # @paginated_orders = @store_orders.paginate(:page => params[:page], :per_page => 50)
+    #
+    # @orders = @paginated_orders.paginate(:page => params[:page], :per_page => 50)
   end
 
   def paid
@@ -20,5 +25,11 @@ class Admin::OrdersController < ApplicationController
 
   def cancelled
     @orders = Order.cancelled
+  end
+
+  private
+
+  def set_page
+    @page = params[:page] || 0
   end
 end
