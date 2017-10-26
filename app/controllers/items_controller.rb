7571edcdc5ecbe7edd4b2888d54e5815a8cbcc24
Ashish Vaid <vaid50@yahoo.com>
2017-10-26 10:36:45 -0600

    items index pagination style now centered

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-    @items = Item.paginate(:page => params[:page], :per_page => 10)
+    @items = Item.paginate(:page => params[:page], :per_page => 50)
   end
 
   def show
